65b84468b769d6747ca7742dd6b02f691b2d521e
YamilGhazi <yghazi@gmail.com>
2013-02-25 18:23:24 +0100

    Derp
    Change-Id: I6e873c1dd434ba88eb4520a3ae7d9ef9860fead4

--- src/com/android/settings/DevelopmentSettings.java
+++ src/com/android/settings/DevelopmentSettings.java
@@ -243,7 +243,7 @@ public class DevelopmentSettings extends PreferenceFragment
         mPassword = (PreferenceScreen) findPreference(LOCAL_BACKUP_PASSWORD);
         mAllPrefs.add(mPassword);
         mAdvancedReboot = findAndInitCheckboxPref(ADVANCED_REBOOT_KEY);
-        updateAdvancedRebootOptions()
+        updateAdvancedRebootOptions();
 
         if (!android.os.Process.myUserHandle().equals(UserHandle.OWNER)) {
             disableForUser(mEnableAdb);
