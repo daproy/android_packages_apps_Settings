8b9d665a9b7732552d72b7fc5536d6753cea7f91
YamilGhazi <yghazi@gmail.com>
2013-02-25 08:10:39 +0100

    Owner is always a dev
    Change-Id: Iefdabb2fee65f64eca6297467e49f049bbce3afb

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -422,9 +422,7 @@ public class Settings extends PreferenceActivity
     }
 
     private void updateHeaderList(List<Header> target) {
-        final boolean showDev = mDevelopmentPreferences.getBoolean(
-                DevelopmentSettings.PREF_SHOW,
-                android.os.Build.TYPE.equals("eng"));
+        final boolean showDev = (UserHandle.myUserId() == UserHandle.USER_OWNER);
         int i = 0;
 
         mHeaderIndexMap.clear();
