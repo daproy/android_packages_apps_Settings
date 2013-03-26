b5e241f515972b4f60a43a23fd931b7e09a89efd
benjamingoodwin <benjamin.j.goodwin@gmail.com>
2013-03-26 20:44:15 +0000

    last app

--- src/com/android/settings/aokp/Navbar.java
+++ src/com/android/settings/aokp/Navbar.java
@@ -589,6 +589,8 @@ public class Navbar extends SettingsPreferenceFragment implements
                 return getResources().getDrawable(R.drawable.ic_sysbar_power);
             } else if (uri.equals("**notifications**")) {
                 return getResources().getDrawable(R.drawable.ic_sysbar_notifications);
+            } else if (uri.equals("**lastapp**")) {
+                return getResources().getDrawable(R.drawable.ic_sysbar_lastapp);
             }
         } else {
             try {
@@ -635,6 +637,8 @@ public class Navbar extends SettingsPreferenceFragment implements
                 return getResources().getString(R.string.navbar_action_power);
             else if (uri.equals("**notifications**"))
                 return getResources().getString(R.string.navbar_action_notifications);
+            else if (uri.equals("**lastapp**"))
+                return getResources().getString(R.string.navbar_action_lastapp);
             else if (uri.equals("**null**"))
                 return getResources().getString(R.string.navbar_action_none);
         } else {
