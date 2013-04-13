23fb86d4ff07648b00e95707c503802812c8756f
benjamingoodwin <benjamin.j.goodwin@gmail.com>
2013-04-13 12:40:01 +0100

    Use our own ColorPicker and UI Restart to Set Values

--- src/com/android/settings/cyanogenmod/PieControl.java
+++ src/com/android/settings/cyanogenmod/PieControl.java
@@ -1,5 +1,6 @@
 package com.android.settings.cyanogenmod;
 
+import android.content.Context;
 import android.content.ContentResolver;
 import android.database.ContentObserver;
 import android.os.Bundle;
@@ -12,12 +13,14 @@ import android.provider.Settings;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.hybrid.Utils;
+
+import com.android.settings.cyanogenmod.colorpicker.ColorPickerPreference;
 
 public class PieControl extends SettingsPreferenceFragment
                         implements Preference.OnPreferenceChangeListener {
 
     private static final int DEFAULT_POSITION = 1 << 1; // this equals Position.BOTTOM.FLAG
-
     private static final String PIE_CONTROL = "pie_control_checkbox";
     private static final String PIE_SIZE = "pie_control_size";
     private static final String[] TRIGGER = {
@@ -26,108 +29,93 @@ public class PieControl extends SettingsPreferenceFragment
         "pie_control_trigger_right",
         "pie_control_trigger_top"
     };
-
+ 
     private CheckBoxPreference mPieControl;
     private SeekBarDialogPreference mPieSize;
     private CheckBoxPreference[] mTrigger = new CheckBoxPreference[4];
-    private Preference mPieColor;
-    private Preference mPieSelectedColor;
-    private Preference mPieOutlineColor;
+    ColorPickerPreference mPieColor;
+    ColorPickerPreference mPieSelectedColor;
+    ColorPickerPreference mPieOutlineColor;
 
     private ContentObserver mPieTriggerObserver = new ContentObserver(new Handler()) {
         @Override
         public void onChange(boolean selfChange) {
             updatePieTriggers();
+            refreshSettings();
         }
     };
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        refreshSettings();
+    }
+
+    public void refreshSettings() {
+        PreferenceScreen prefs = getPreferenceScreen();
+        if (prefs != null) {
+            prefs.removeAll();
+        }
 
         addPreferencesFromResource(R.xml.pie_control);
 
-        PreferenceScreen prefSet = getPreferenceScreen();
-        mPieControl = (CheckBoxPreference) prefSet.findPreference(PIE_CONTROL);
+        prefs = getPreferenceScreen();
+
+        mPieControl = (CheckBoxPreference) prefs.findPreference(PIE_CONTROL);
         mPieControl.setOnPreferenceChangeListener(this);
-        mPieSize = (SeekBarDialogPreference) prefSet.findPreference(PIE_SIZE);
-        mPieColor = prefSet.findPreference("pie_color");
-        mPieSelectedColor = prefSet.findPreference("pie_selected_color");
-        mPieOutlineColor = prefSet.findPreference("pie_outline_color");
+        mPieSize = (SeekBarDialogPreference) prefs.findPreference(PIE_SIZE);
+        mPieColor = (ColorPickerPreference) prefs.findPreference("pie_color");
+        mPieColor.setOnPreferenceChangeListener(this);
+        mPieSelectedColor = (ColorPickerPreference) prefs.findPreference("pie_selected_color");
+        mPieSelectedColor.setOnPreferenceChangeListener(this);
+        mPieOutlineColor = (ColorPickerPreference) prefs.findPreference("pie_outline_color");
+        mPieOutlineColor.setOnPreferenceChangeListener(this);
 
         for (int i = 0; i < TRIGGER.length; i++) {
-            mTrigger[i] = (CheckBoxPreference) prefSet.findPreference(TRIGGER[i]);
+            mTrigger[i] = (CheckBoxPreference) prefs.findPreference(TRIGGER[i]);
             mTrigger[i].setOnPreferenceChangeListener(this);
         }
     }
 
+    @Override
     public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
-        boolean value;
-
-        if (preference == mPieColor) {
-            ColorPickerDialog cp = new ColorPickerDialog(getActivity(),
-                    mPieColorListener, Settings.System.getInt(getContentResolver(),
-                    Settings.System.PIE_COLOR, 0xdd0099cc));
-            cp.setDefaultColor(0xdd0099cc);
-            cp.show();
-            return true;
-        } else if (preference == mPieSelectedColor) {
-            ColorPickerDialog cp = new ColorPickerDialog(getActivity(),
-                    mPieSelectedColorListener, Settings.System.getInt(getContentResolver(),
-                    Settings.System.PIE_SELECTED_COLOR, 0xff33b5e5));
-            cp.setDefaultColor(0xff33b5e5);
-            cp.show();
-            return true;
-        } else if (preference == mPieOutlineColor) {
-            ColorPickerDialog cp = new ColorPickerDialog(getActivity(),
-                    mPieOutlineColorListener, Settings.System.getInt(getContentResolver(),
-                    Settings.System.PIE_OUTLINE_COLOR, 0xdd0099cc));
-            cp.setDefaultColor(0xdd0099cc);
-            cp.show();
-            return true;
-        }
+        boolean value;        
         return super.onPreferenceTreeClick(preferenceScreen, preference);
     }
 
-    ColorPickerDialog.OnColorChangedListener mPieColorListener =
-        new ColorPickerDialog.OnColorChangedListener() {
-            public void colorChanged(int color) {
-                Settings.System.putInt(getContentResolver(),
-                        Settings.System.PIE_COLOR, color);
-            }
-            public void colorUpdate(int color) {
-            }
-    };
-
-    ColorPickerDialog.OnColorChangedListener mPieSelectedColorListener =
-        new ColorPickerDialog.OnColorChangedListener() {
-            public void colorChanged(int color) {
-                Settings.System.putInt(getContentResolver(),
-                        Settings.System.PIE_SELECTED_COLOR, color);
-            }
-            public void colorUpdate(int color) {
-            }
-    };
-
-    ColorPickerDialog.OnColorChangedListener mPieOutlineColorListener =
-        new ColorPickerDialog.OnColorChangedListener() {
-            public void colorChanged(int color) {
-                Settings.System.putInt(getContentResolver(),
-                        Settings.System.PIE_OUTLINE_COLOR, color);
-            }
-            public void colorUpdate(int color) {
-            }
-    };
-
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
+
         if (preference == mPieControl) {
             boolean newState = (Boolean) newValue;
 
             Settings.System.putInt(getContentResolver(),
                     Settings.System.PIE_CONTROLS, newState ? 1 : 0);
             propagatePieControl(newState);
-
+        } else if (preference == mPieColor) {
+           String hex = ColorPickerPreference.convertToARGB(
+                    Integer.valueOf(String.valueOf(newValue)));
+            preference.setSummary(hex);
+            int intHex = ColorPickerPreference.convertToColorInt(hex);
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.PIE_COLOR, intHex);
+            Utils.restartUI(getActivity());
+        } else if (preference == mPieSelectedColor) {
+            String hex = ColorPickerPreference.convertToARGB(
+                    Integer.valueOf(String.valueOf(newValue)));
+            preference.setSummary(hex);
+            int intHex = ColorPickerPreference.convertToColorInt(hex);
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.PIE_SELECTED_COLOR, intHex);
+            Utils.restartUI(getActivity());
+        } else if (preference == mPieOutlineColor) {
+            String hex = ColorPickerPreference.convertToARGB(
+                    Integer.valueOf(String.valueOf(newValue)));
+            preference.setSummary(hex);
+            int intHex = ColorPickerPreference.convertToColorInt(hex);
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.PIE_OUTLINE_COLOR, intHex);
+            Utils.restartUI(getActivity());
         } else {
             int triggerSlots = 0;
             for (int i = 0; i < mTrigger.length; i++) {
