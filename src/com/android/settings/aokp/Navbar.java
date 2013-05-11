fd787668351bdaa91e7a904d097a6f41c6a34cd5
beerbong <yghazi@gmail.com>
2013-05-11 12:28:30 +0200

    Revert "Disable navbar height when in tablet ui"
    This reverts commit bc37d54a82d1321dd544c14477524c4b8728fc94.

--- src/com/android/settings/aokp/Navbar.java
+++ src/com/android/settings/aokp/Navbar.java
@@ -134,10 +134,7 @@ public class Navbar extends SettingsPreferenceFragment implements
 
         PreferenceScreen prefs = getPreferenceScreen();
 
-        boolean isTablet = ExtendedPropertiesUtils.isTablet();
-
         mNavbarHeight = findPreference("navbar_height");
-        mNavbarHeight.setEnabled(!isTablet);
 
         mPicker = new ShortcutPickerHelper(this, this);
 
