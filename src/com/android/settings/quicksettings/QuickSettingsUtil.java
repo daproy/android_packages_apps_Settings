8490159bacbb35e511d9f35c5672d218f7fcb401
lion0738 <lion0738@naver.com>
2013-12-06 12:17:43 +0900

    Add music tile from SlimRom (2/2)
    Change-Id: I4716fff82da5f03c6942cab950383e8828183f6d

--- src/com/android/settings/quicksettings/QuickSettingsUtil.java
+++ src/com/android/settings/quicksettings/QuickSettingsUtil.java
@@ -30,6 +30,7 @@ import static com.android.internal.util.cm.QSConstants.TILE_GPS;
 import static com.android.internal.util.cm.QSConstants.TILE_LOCKSCREEN;
 import static com.android.internal.util.cm.QSConstants.TILE_LTE;
 import static com.android.internal.util.cm.QSConstants.TILE_MOBILEDATA;
+import static com.android.internal.util.cm.QSConstants.TILE_MUSIC;
 import static com.android.internal.util.cm.QSConstants.TILE_NETWORKADB;
 import static com.android.internal.util.cm.QSConstants.TILE_NETWORKMODE;
 import static com.android.internal.util.cm.QSConstants.TILE_NFC;
@@ -160,7 +161,7 @@ public class QuickSettingsUtil {
         registerTile(new QuickSettingsUtil.TileInfo(
                 TILE_NETWORKADB, R.string.title_tile_network_adb,
                 "com.android.systemui:drawable/ic_qs_network_adb_off"));
-	registerTile(new QuickSettingsUtil.TileInfo(
+		registerTile(new QuickSettingsUtil.TileInfo(
                 TILE_POWER, R.string.title_tile_power,
                 "com.android.systemui:drawable/ic_qs_powermenu")); 
         registerTile(new QuickSettingsUtil.TileInfo(
@@ -169,6 +170,9 @@ public class QuickSettingsUtil {
         registerTile(new QuickSettingsUtil.TileInfo(
                 TILE_THEME, R.string.title_tile_theme,
                 "com.android.systemui:drawable/ic_qs_theme_manual"));    
+        registerTile(new QuickSettingsUtil.TileInfo(
+                TILE_MUSIC, R.string.title_tile_music,
+                "com.android.systemui:drawable/ic_qs_media_play"));
     }
 
     private static void registerTile(QuickSettingsUtil.TileInfo info) {
