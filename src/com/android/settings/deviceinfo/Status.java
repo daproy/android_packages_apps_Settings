1ac786741d2e3eaa8908f9ecfe045127461d60f1
Pawit Pornkitprasan <p.pawit@gmail.com>
2014-01-08 22:42:38 +0700

    Settings: use CMHW serial number
    See http://review.cyanogenmod.org/57164
    
    Change-Id: If328ca7b487435b88246d1f8c47b04770183f09e
    
    Conflicts:
    	src/com/android/settings/deviceinfo/Status.java

--- src/com/android/settings/deviceinfo/Status.java
+++ src/com/android/settings/deviceinfo/Status.java
@@ -60,6 +60,8 @@ import com.android.settings.R;
 import com.android.settings.SelectSubscription;
 import com.android.settings.Utils;
 
+import org.cyanogenmod.hardware.SerialNumber;
+
 import java.lang.ref.WeakReference;
 
 /**
@@ -381,7 +383,7 @@ public class Status extends PreferenceActivity {
 
         updateConnectivity();
 
-        String serial = Build.SERIAL;
+        String serial = getSerialNumber();
         if (serial != null && !serial.equals("")) {
             setSummaryText(KEY_SERIAL_NUMBER, serial);
         } else {
@@ -668,6 +670,18 @@ public class Status extends PreferenceActivity {
         return (TelephonyManager.getDefault().getPhoneCount() > 1);
     }
 
+    private String getSerialNumber() {
+        try {
+            if (SerialNumber.isSupported()) {
+                return SerialNumber.getSerialNumber();
+            }
+        } catch (NoClassDefFoundError e) {
+            // Hardware abstraction framework not installed; fall through
+        }
+
+        return Build.SERIAL;
+    }
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         final int itemId = item.getItemId();
@@ -686,5 +700,4 @@ public class Status extends PreferenceActivity {
     private static void goUpToTopLevelSetting(Activity activity) {
         activity.finish();
     }
-
 }
