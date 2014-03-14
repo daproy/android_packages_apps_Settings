fbd3479e2313f5f45c8d4a6cdb58de5d06b2877c
Janson Kang <temasek71@gmail.com>
2014-03-14 08:38:06 +0800

    Revert "Settings: Fix value from being overwritten with null."
    This reverts commit e2c8309973316336d835495477ff832d19abbc00.

--- src/com/android/settings/ApnSettings.java
+++ src/com/android/settings/ApnSettings.java
@@ -138,7 +138,7 @@ public class ApnSettings extends SettingsPreferenceFragment implements
         if (MSimTelephonyManager.getDefault().isMultiSimEnabled()) {
             mPreferApnUri = Uri.parse(PREFERRED_APN_URI + "/" + mSubscription);
         } else {
-            mPreferApnUri = PREFERAPN_URI;
+            mPreferApnUri = Uri.parse(PREFERRED_APN_URI);
         }
         Log.d(TAG, "Preferred APN Uri is set to '" + mPreferApnUri.toString() + "'");
 
