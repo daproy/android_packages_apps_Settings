171cf95052e4540ae7e41c9d6657808d95531bde
Roman Birg <roman@cyngn.com>
2014-03-26 16:14:58 -0700

    add Voice Wakeup switch
     * Requires CYNGN package to do the actual work.
    
    Change-Id: I1c129adf1104dd95d3afb76224af4c9ba2b5db96
    
    Conflicts:
    	proguard.flags
    	src/com/android/settings/cyanogenmod/ShortcutPickHelper.java

--- src/com/android/settings/DevelopmentSettings.java
+++ src/com/android/settings/DevelopmentSettings.java
@@ -337,7 +337,7 @@ public class DevelopmentSettings extends SettingsPreferenceFragment
         }
         mAllPrefs.add(mClearAdbKeys);
         mEnableTerminal = findAndInitSwitchPref(ENABLE_TERMINAL);
-        if (!isPackageInstalled(getActivity(), TERMINAL_APP_PACKAGE)) {
+        if (!Utils.isPackageInstalled(getActivity(), TERMINAL_APP_PACKAGE)) {
             debugDebuggingCategory.removePreference(mEnableTerminal);
             mEnableTerminal = null;
         }
@@ -369,7 +369,7 @@ public class DevelopmentSettings extends SettingsPreferenceFragment
             disableForUser(mQuickBoot);
         }
 
-        if (!isPackageInstalled(getActivity(), QUICKBOOT_PACKAGE_NAME)) {
+        if (!Utils.isPackageInstalled(getActivity(), QUICKBOOT_PACKAGE_NAME)) {
             removePreference(mQuickBoot);
         }
 
@@ -1943,14 +1943,6 @@ public class DevelopmentSettings extends SettingsPreferenceFragment
                 }
             }
             return null;
-        }
-    }
-
-    private static boolean isPackageInstalled(Context context, String packageName) {
-        try {
-            return context.getPackageManager().getPackageInfo(packageName, 0) != null;
-        } catch (NameNotFoundException e) {
-            return false;
         }
     }
 
