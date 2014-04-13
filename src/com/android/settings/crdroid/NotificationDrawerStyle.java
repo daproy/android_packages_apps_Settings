3d759dae14bf12620f50f81de69524cdad6e6e50
Jubakuba <Jubakuba@gmail.com>
2014-04-13 15:54:27 -0300

    Settings: Clean up SeekBar
    Change-Id: I564d4d2454929e87d4eea2ebf4ef27792666e759
    
    Conflicts:
    
    	res/values/cr_strings.xml

--- src/com/android/settings/crdroid/NotificationDrawerStyle.java
+++ src/com/android/settings/crdroid/NotificationDrawerStyle.java
@@ -110,7 +110,6 @@ public class NotificationDrawerStyle extends SettingsPreferenceFragment implemen
         }
         mWallpaperAlpha = (SeekBarPreference) findPreference(PREF_NOTIFICATION_WALLPAPER_ALPHA);
         mWallpaperAlpha.setInitValue((int) (transparency * 100));
-        mWallpaperAlpha.setProperty(Settings.System.NOTIFICATION_BACKGROUND_ALPHA);
         mWallpaperAlpha.setOnPreferenceChangeListener(this);
 
         try{
@@ -123,7 +122,6 @@ public class NotificationDrawerStyle extends SettingsPreferenceFragment implemen
         }
         mNotificationAlpha = (SeekBarPreference) findPreference(PREF_NOTIFICATION_ALPHA);
         mNotificationAlpha.setInitValue((int) (transparency * 100));
-        mNotificationAlpha.setProperty(Settings.System.NOTIFICATION_ALPHA);
         mNotificationAlpha.setOnPreferenceChangeListener(this);
 
         updateCustomBackgroundSummary();
--- src/com/android/settings/crdroid/SeekBarPreference.java
+++ src/com/android/settings/crdroid/SeekBarPreference.java
@@ -19,15 +19,17 @@ public class SeekBarPreference extends Preference
         implements OnSeekBarChangeListener {
 
     public static int maximum = 100;
-    public static int interval = 5;
-
-    private String property;
+    public int interval = 5;
 
     private TextView monitorBox;
     private SeekBar bar;
 
     int defaultValue = 60;
+    int mSetDefault = -1;
+    int mMultiply = -1;
+    int mMinimum = -1;
     boolean mDisablePercentageValue = false;
+    boolean mIsMilliSeconds = false;
 
     private OnPreferenceChangeListener changer;
 
@@ -42,28 +44,16 @@ public class SeekBarPreference extends Preference
 
         monitorBox = (TextView) layout.findViewById(R.id.monitor_box);
         bar = (SeekBar) layout.findViewById(R.id.seek_bar);
-        int progress;
-        try{
-            progress = (int) (Settings.System.getFloat(
-                    getContext().getContentResolver(), property) * 100);
-        } catch (Exception e) {
-            progress = defaultValue;
-        }
         bar.setOnSeekBarChangeListener(this);
-        bar.setProgress(progress);
-        if (!mDisablePercentageValue) {
-            monitorBox.setText(progress + "%");
-        }
+        bar.setProgress(defaultValue);
+
         return layout;
     }
 
     public void setInitValue(int progress) {
         defaultValue = progress;
-        if (bar!=null) {
-            bar.setProgress(progress);
-            if (!mDisablePercentageValue) {
-                monitorBox.setText(progress + "%");
-            }
+        if (bar != null) {
+            bar.setProgress(defaultValue);
         }
     }
 
@@ -86,28 +76,50 @@ public class SeekBarPreference extends Preference
         progress = Math.round(((float) progress) / interval) * interval;
         seekBar.setProgress(progress);
 
-        if (!mDisablePercentageValue) {
-            monitorBox.setText(progress + "%");
+        if (mMultiply != -1) {
+            progress = progress * mMultiply;
+        }
+
+        if (mMinimum != -1) {
+            progress += mMinimum;
+        }
+
+        if (progress == mSetDefault) {
+            monitorBox.setText(R.string.default_string);
+        } else {
+            if (mIsMilliSeconds) {
+                monitorBox.setText(progress + " ms");
+            } else if (!mDisablePercentageValue) {
+                monitorBox.setText(progress + "%");
+            } else {
+                monitorBox.setText(progress);
+            }
         }
         changer.onPreferenceChange(this, Integer.toString(progress));
-    }
-
-    public void setValue(int progress){
-        if (bar!=null) {
-            bar.setProgress(progress);
-            if (!mDisablePercentageValue) {
-                monitorBox.setText(progress + "%");
-            }
-            changer.onPreferenceChange(this, Integer.toString(progress));
-        }
     }
 
     public void disablePercentageValue(boolean disable) {
         mDisablePercentageValue = disable;
     }
 
-    public void setProperty(String property) {
-        this.property = property;
+    public void setInterval(int inter) {
+        interval = inter;
+    }
+
+    public void setDefault(int defaultVal) {
+        mSetDefault = defaultVal;
+    }
+
+    public void multiplyValue(int val) {
+        mMultiply = val;
+    }
+
+    public void minimumValue(int val) {
+        mMinimum = val;
+    }
+
+    public void isMilliseconds(boolean millis) {
+        mIsMilliSeconds = millis;
     }
 
     @Override
