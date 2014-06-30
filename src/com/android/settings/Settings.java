f260d92c0cc45ce57ed4b83aa45a538fb91aa654
Adnan <adnan@cyngn.com>
2014-06-30 16:10:00 -0700

    Settings: Check to see if menu items are null onPause.
    Change-Id: I5463b1eab1017840ec1ec204090c9c61603377a3

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -416,8 +416,14 @@ public class Settings extends PreferenceActivity
     @Override
     public void onPause() {
         super.onPause();
-        mSearchBar.clearFocus();
-        mSearchItem.collapseActionView();
+
+        if (mSearchBar != null) {
+            mSearchBar.clearFocus();
+        }
+
+        if (mSearchItem != null) {
+            mSearchItem.collapseActionView();
+        }
 
         try {
             unregisterReceiver(mBatteryInfoReceiver);
