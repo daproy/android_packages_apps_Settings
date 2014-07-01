5031ec0e517d53fabbc7d94a1686b3080fb0daf6
Danesh M <daneshm90@gmail.com>
2014-07-01 11:01:59 -0700

    Settings : Get actionbar post extra retrieval
    Fetching the actionbar before the extras are utilized causes the parentActivityName
    to be null and no home as up enabled.
    
    Change-Id: Ic3395c4a2e0c04aee4efab5345cb604475691016

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -313,9 +313,6 @@ public class Settings extends PreferenceActivity
 
         startPopulatingSearchData();
 
-        mActionBar = getActionBar();
-        mActionBar.setDisplayShowCustomEnabled(true);
-
         mAuthenticatorHelper = new AuthenticatorHelper();
         mAuthenticatorHelper.updateAuthDescriptions(this);
         mAuthenticatorHelper.onAccountsUpdated(this, null);
@@ -361,6 +358,9 @@ public class Settings extends PreferenceActivity
             getActionBar().setDisplayHomeAsUpEnabled(false);
             getActionBar().setHomeButtonEnabled(false);
         }
+
+        mActionBar = getActionBar();
+        mActionBar.setDisplayShowCustomEnabled(true);
     }
 
     @Override
