77a333ed4c6cc8cb6406ca2bdfc11aabc0978857
Lars Greiss <kufikugel@googlemail.com>
2014-07-31 17:49:10 -0300

    Settings: Show heads up at the bottom of the screen (2/2) Due of user requests and indeed especially on hw key devices for a better workflow we add an option to show heads up notifications on the bottom instead of the top.
    Animation we leave as is due that if it slide from the bottom into the screen it happens
    to often that the user accidentaly interacts with it. If it comes from the top of the
    container we add, the user has enough time to realize it. Sometimes a ms can make a big difference.

--- src/com/android/settings/crdroid/HeadsUpSettings.java
+++ src/com/android/settings/crdroid/HeadsUpSettings.java
@@ -32,11 +32,13 @@ public class HeadsUpSettings extends SettingsPreferenceFragment
     private static final String PREF_HEADS_UP_SNOOZE_TIME = "heads_up_snooze_time";
     private static final String PREF_HEADS_UP_TIME_OUT = "heads_up_time_out";
     private static final String PREF_HEADS_UP_SHOW_UPDATE = "heads_up_show_update";
+    private static final String PREF_HEADS_UP_GRAVITY = "heads_up_gravity";
     
     ListPreference mHeadsUpSnoozeTime;
     ListPreference mHeadsUpTimeOut;
     CheckBoxPreference mHeadsUpExpanded;
     CheckBoxPreference mHeadsUpShowUpdates;
+    CheckBoxPreference mHeadsUpGravity;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -57,6 +59,11 @@ public class HeadsUpSettings extends SettingsPreferenceFragment
         mHeadsUpShowUpdates.setChecked(Settings.System.getIntForUser(getContentResolver(),
                 Settings.System.HEADS_UP_SHOW_UPDATE, 0, UserHandle.USER_CURRENT) == 1);
         mHeadsUpShowUpdates.setOnPreferenceChangeListener(this);
+
+        mHeadsUpGravity = (CheckBoxPreference) findPreference(PREF_HEADS_UP_GRAVITY);
+        mHeadsUpGravity.setChecked(Settings.System.getIntForUser(getContentResolver(),
+                Settings.System.HEADS_UP_GRAVITY_BOTTOM, 0, UserHandle.USER_CURRENT) == 1);
+        mHeadsUpGravity.setOnPreferenceChangeListener(this);
 
         mHeadsUpSnoozeTime = (ListPreference) findPreference(PREF_HEADS_UP_SNOOZE_TIME);
         mHeadsUpSnoozeTime.setOnPreferenceChangeListener(this);
@@ -112,6 +119,11 @@ public class HeadsUpSettings extends SettingsPreferenceFragment
         } else if (preference == mHeadsUpShowUpdates) {
             Settings.System.putIntForUser(getContentResolver(),
                     Settings.System.HEADS_UP_SHOW_UPDATE,
+                    (Boolean) newValue ? 1 : 0, UserHandle.USER_CURRENT);
+            return true;
+        } else if (preference == mHeadsUpGravity) {
+            Settings.System.putIntForUser(getContentResolver(),
+                    Settings.System.HEADS_UP_GRAVITY_BOTTOM,
                     (Boolean) newValue ? 1 : 0, UserHandle.USER_CURRENT);
             return true;
         }
