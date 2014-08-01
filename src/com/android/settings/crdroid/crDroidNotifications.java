d32044313df766d613244c91a438aa4cbbfe0a14
cristianomatos <cristianobmatos@gmail.com>
2014-08-01 12:13:55 -0300

    Put all notification services in one place
    Change-Id: I865a2cea5e25d59fc1a78041878bc48c3bf024be
    
    Conflicts:
    	res/values/cr_strings.xml
    	res/xml/settings_headers.xml

---
+++ src/com/android/settings/crdroid/crDroidNotifications.java
@@ -0,0 +1,36 @@
+package com.android.settings.crdroid;
+
+import android.app.ActivityManager;
+import android.content.Context;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.preference.CheckBoxPreference;
+import android.preference.Preference;
+import android.preference.PreferenceScreen;
+import android.preference.PreferenceCategory;
+import android.preference.Preference.OnPreferenceChangeListener;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+
+public class crDroidNotifications extends SettingsPreferenceFragment implements
+        Preference.OnPreferenceChangeListener {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.crdroid_notifications);
+
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        return false;
+    }
+}
