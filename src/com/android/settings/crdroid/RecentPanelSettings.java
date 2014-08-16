c98d4ff132c84caeb341ea0e594118ffdef18ee7
Daniel Koman <dankoman30@gmail.com>
2014-08-15 21:07:56 -0300

    Settings: Slimrecents background color && transparency [1/2] Allow user to specify background color and transparency level     for the slimrecents panel.
    PS: now setting sticks through reboots and TRDS state
            changes. also, add reset to default color option.
    
    PS: when resetting to default, use TRDS (png) resources
            instead of custom color
    
    NOT USED FOR crDroid -> PS: properly set preference summary for default
            value ("TRDS default")
    
    Change-Id: I42ec2c9cf0f65b0dc8936e1cc45f171afe1d6afa
    PS: remove LSN dependency
    
    Conflicts:
    	src/com/android/settings/crdroid/RecentPanelSettings.java

--- src/com/android/settings/crdroid/RecentPanelSettings.java
+++ src/com/android/settings/crdroid/RecentPanelSettings.java
@@ -41,10 +41,11 @@ public class RecentPanelSettings extends SettingsPreferenceFragment implements O
     private static final String RAM_BAR_COLOR_CACHE_MEM = "ram_bar_color_cache_mem";
     private static final String RAM_BAR_COLOR_TOTAL_MEM = "ram_bar_color_total_mem";
     // Slim recent
-    private static final String RECENT_PANEL_SHOW_TOPMOST = "recent_panel_show_topmost";
     private static final String RECENT_PANEL_LEFTY_MODE = "recent_panel_lefty_mode";
     private static final String RECENT_PANEL_SCALE = "recent_panel_scale";
     private static final String RECENT_PANEL_EXPANDED_MODE = "recent_panel_expanded_mode";
+    private static final String RECENT_PANEL_SHOW_TOPMOST = "recent_panel_show_topmost";
+    private static final String RECENT_PANEL_BG_COLOR = "recent_panel_bg_color";
 
     private static final int MENU_RESET = Menu.FIRST;
     private static final int MENU_HELP = MENU_RESET + 1; 
@@ -61,10 +62,13 @@ public class RecentPanelSettings extends SettingsPreferenceFragment implements O
     private ColorPickerPreference mRamBarCacheMemColor;
     private ColorPickerPreference mRamBarTotalMemColor;
     // Slim recent
-    private CheckBoxPreference mRecentsShowTopmost;
     private CheckBoxPreference mRecentPanelLeftyMode;
     private ListPreference mRecentPanelScale;
     private ListPreference mRecentPanelExpandedMode;
+    private CheckBoxPreference mRecentsShowTopmost;
+    private ColorPickerPreference mRecentPanelBgColor;
+
+    private static final int DEFAULT_BACKGROUND_COLOR = 0x00ffffff;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -127,11 +131,6 @@ public class RecentPanelSettings extends SettingsPreferenceFragment implements O
         mRamBarTotalMemColor.setNewPreviewColor(intColor);
 
         // Slim recent
-        boolean enableRecentsShowTopmost = Settings.System.getInt(getContentResolver(),
-                                      Settings.System.RECENT_PANEL_SHOW_TOPMOST, 0) == 1;
-        mRecentsShowTopmost = (CheckBoxPreference) findPreference(RECENT_PANEL_SHOW_TOPMOST);
-        mRecentsShowTopmost.setChecked(enableRecentsShowTopmost);
-        mRecentsShowTopmost.setOnPreferenceChangeListener(this);
         mRecentPanelLeftyMode = (CheckBoxPreference) findPreference(RECENT_PANEL_LEFTY_MODE);
         mRecentPanelLeftyMode.setOnPreferenceChangeListener(this);
         mRecentPanelScale = (ListPreference) findPreference(RECENT_PANEL_SCALE);
@@ -142,6 +141,25 @@ public class RecentPanelSettings extends SettingsPreferenceFragment implements O
         final int recentExpandedMode = Settings.System.getInt(getContentResolver(),
                 Settings.System.RECENT_PANEL_EXPANDED_MODE, 0);
         mRecentPanelExpandedMode.setValue(recentExpandedMode + "");
+
+        boolean enableRecentsShowTopmost = Settings.System.getInt(getContentResolver(),
+                                      Settings.System.RECENT_PANEL_SHOW_TOPMOST, 0) == 1;
+        mRecentsShowTopmost = (CheckBoxPreference) findPreference(RECENT_PANEL_SHOW_TOPMOST);
+        mRecentsShowTopmost.setChecked(enableRecentsShowTopmost);
+        mRecentsShowTopmost.setOnPreferenceChangeListener(this);
+
+        // Recent panel background color
+        mRecentPanelBgColor = (ColorPickerPreference) findPreference(RECENT_PANEL_BG_COLOR);
+        mRecentPanelBgColor.setOnPreferenceChangeListener(this);
+        intColor = Settings.System.getInt(getContentResolver(),
+                Settings.System.RECENT_PANEL_BG_COLOR, 0x00ffffff);
+        hexColor = String.format("#%08x", (0x00ffffff & intColor));
+        //if (hexColor.equals("#00ffffff")) {
+        //    mRecentPanelBgColor.setSummary("TRDS default");
+        //} else {
+            mRecentPanelBgColor.setSummary(hexColor);
+        //}
+        mRecentPanelBgColor.setNewPreviewColor(intColor);
 
         updateRecentsOptions();
         setHasOptionsMenu(true);
@@ -160,6 +178,7 @@ public class RecentPanelSettings extends SettingsPreferenceFragment implements O
         switch (item.getItemId()) {
             case MENU_RESET:
                 resetToDefault();
+                resetValues();
                 return true;
             default: 
                 return super.onContextItemSelected(item);
@@ -173,11 +192,12 @@ public class RecentPanelSettings extends SettingsPreferenceFragment implements O
         alertDialog.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
             public void onClick(DialogInterface dialog, int id) {
                 ramBarColorReset();
+                resetValues();
             }
         });
         alertDialog.setNegativeButton(R.string.cancel, null);
         alertDialog.create().show();
-    } 
+    }
 
     public boolean onPreferenceChange(Preference preference, Object newValue) {
         if (preference == mRecentsCustom) { // Enable||disbale Slim Recent
@@ -237,11 +257,6 @@ public class RecentPanelSettings extends SettingsPreferenceFragment implements O
             Settings.System.putInt(getContentResolver(),
                     Settings.System.RECENT_PANEL_SCALE_FACTOR, value);
             return true;
-        } else if (preference == mRecentsShowTopmost) {
-            Settings.System.putInt(getContentResolver(),
-                    Settings.System.RECENT_PANEL_SHOW_TOPMOST,
-                    ((Boolean) newValue) ? 1 : 0);
-            return true;
         } else if (preference == mRecentPanelLeftyMode) {
             Settings.System.putInt(getContentResolver(),
                     Settings.System.RECENT_PANEL_GRAVITY,
@@ -252,8 +267,33 @@ public class RecentPanelSettings extends SettingsPreferenceFragment implements O
             Settings.System.putInt(getContentResolver(),
                     Settings.System.RECENT_PANEL_EXPANDED_MODE, value);
             return true;
+        } else if (preference == mRecentsShowTopmost) {
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.RECENT_PANEL_SHOW_TOPMOST,
+                    ((Boolean) newValue) ? 1 : 0);
+            return true;
+        } else if (preference == mRecentPanelBgColor) {
+            String hex = ColorPickerPreference.convertToARGB(
+                    Integer.valueOf(String.valueOf(newValue)));
+            //if (hex.equals("#00ffffff")) {
+            //    preference.setSummary("TRDS default");
+            //} else {
+                preference.setSummary(hex);
+            //}
+            int intHex = ColorPickerPreference.convertToColorInt(hex);
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.RECENT_PANEL_BG_COLOR,
+                    intHex);
+            return true;
         }
         return false;
+    }
+
+    private void resetValues() {
+        Settings.System.putInt(getContentResolver(),
+                Settings.System.RECENT_PANEL_BG_COLOR, DEFAULT_BACKGROUND_COLOR);
+        mRecentPanelBgColor.setNewPreviewColor(DEFAULT_BACKGROUND_COLOR);
+        //mRecentPanelBgColor.setSummary("TRDS default");
     }
 
     private void ramBarColorReset() {
@@ -274,7 +314,6 @@ public class RecentPanelSettings extends SettingsPreferenceFragment implements O
         hexColor = String.format("#%08x", (0xffffffff & DEFAULT_MEM_COLOR));
         mRamBarTotalMemColor.setSummary(hexColor);
     }
-
 
     private void updateRecentsOptions() {
         int ramBarMode = Settings.System.getInt(getActivity().getContentResolver(),
