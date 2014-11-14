14dbb0b5df1d0c3717d5620ebc854197c82f03b8
Nextbit <contributions@nextbit.com>
2014-11-14 12:30:32 -0800

    Remove the "Pass widget" config from Settings
    Since the Pass widget is an app-specific feature (specifically Baton's),
    the option to toggle the widget should be presented in the app's
    settings and not in the system settings.
    
    Change-Id: Iec1b4bf4fe25b0d1357c78177f05672c58fe72d3
    
    Conflicts:
    	src/com/android/settings/cyanogenmod/LockscreenInterface.java

--- src/com/android/settings/cyanogenmod/LockscreenInterface.java
+++ src/com/android/settings/cyanogenmod/LockscreenInterface.java
@@ -53,8 +53,6 @@ public class LockscreenInterface extends SettingsPreferenceFragment implements
     private static final String KEY_ENABLE_WIDGETS = "keyguard_enable_widgets";
     private static final String KEY_LOCK_CLOCK = "lock_clock";
     private static final String KEY_ENABLE_CAMERA = "keyguard_enable_camera";
-    private static final String KEY_ENABLE_APPLICATION_WIDGET =
-            "keyguard_enable_application_widget";
     private static final String KEY_ENABLE_MAXIMIZE_WIGETS = "lockscreen_maximize_widgets";
     private static final String KEY_LOCKSCREEN_MODLOCK_ENABLED = "lockscreen_modlock_enabled";
     private static final String KEY_LOCKSCREEN_TARGETS = "lockscreen_targets";
@@ -69,7 +67,6 @@ public class LockscreenInterface extends SettingsPreferenceFragment implements
     private CheckBoxPreference mEnableKeyguardWidgets;
     private CheckBoxPreference mAllWidgets;
     private CheckBoxPreference mEnableCameraWidget;
-    private CheckBoxPreference mEnableApplicationWidget;
     private CheckBoxPreference mEnableModLock;
     private CheckBoxPreference mEnableMaximizeWidgets;
     private ListPreference mBatteryStatus;
@@ -103,7 +100,6 @@ public class LockscreenInterface extends SettingsPreferenceFragment implements
         mEnableKeyguardWidgets = (CheckBoxPreference) findPreference(KEY_ENABLE_WIDGETS);
         mAllWidgets = (CheckBoxPreference) findPreference(KEY_LOCKSCREEN_ALL_WIDGETS);
         mEnableCameraWidget = (CheckBoxPreference) findPreference(KEY_ENABLE_CAMERA);
-        mEnableApplicationWidget = (CheckBoxPreference) findPreference(KEY_ENABLE_APPLICATION_WIDGET);
         mEnableMaximizeWidgets = (CheckBoxPreference) findPreference(KEY_ENABLE_MAXIMIZE_WIGETS);
         mLockscreenTargets = findPreference(KEY_LOCKSCREEN_TARGETS);
 
@@ -164,14 +160,6 @@ public class LockscreenInterface extends SettingsPreferenceFragment implements
                     DevicePolicyManager.KEYGUARD_DISABLE_SECURE_CAMERA);
         }
 
-        // Enable or disable application widget based on policy
-        if (mEnableApplicationWidget != null) {
-            if (!checkDisabledByPolicy(mEnableApplicationWidget,
-                    DevicePolicyManager.KEYGUARD_DISABLE_APPLICATION_WIDGET)) {
-                mEnableApplicationWidget.setEnabled(true);
-            }
-        }
-
         boolean canEnableModLockscreen = false;
         final String keyguardPackage = getActivity().getString(
                 com.android.internal.R.string.config_keyguardPackage);
@@ -206,10 +194,6 @@ public class LockscreenInterface extends SettingsPreferenceFragment implements
         // Update custom widgets and camera
         if (mEnableKeyguardWidgets != null) {
             mEnableKeyguardWidgets.setChecked(mLockUtils.getWidgetsEnabled());
-        }
-
-        if (mEnableApplicationWidget != null) {
-            mEnableApplicationWidget.setChecked(mLockUtils.getApplicationWidgetEnabled());
         }
 
         if (mEnableCameraWidget != null) {
@@ -285,9 +269,6 @@ public class LockscreenInterface extends SettingsPreferenceFragment implements
         } else if (preference == mLockBeforeUnlock) {
             Settings.Secure.putInt(getContentResolver(), Settings.Secure.LOCK_BEFORE_UNLOCK,
                     mLockBeforeUnlock.isChecked() ? 1 : 0);
-        } else if (KEY_ENABLE_APPLICATION_WIDGET.equals(key)) {
-            mLockUtils.setApplicationWidgetEnabled(mEnableApplicationWidget.isChecked());
-            return true;
         }
 
         return super.onPreferenceTreeClick(preferenceScreen, preference);
