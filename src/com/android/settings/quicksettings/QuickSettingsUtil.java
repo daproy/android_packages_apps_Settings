9647f8ee0aebbb5c449bbe86be82b0b685ef9a09
d34d <clark@cyngn.com>
2014-11-15 20:26:45 -0800

    QS: Get your theme on from quick settings [2/2]
    Change-Id: I5073decd1db34b5ab57a2f27896ab4c3e673f4c9
    
    Conflicts:
    	src/com/android/settings/quicksettings/QuickSettingsUtil.java

--- src/com/android/settings/quicksettings/QuickSettingsUtil.java
+++ src/com/android/settings/quicksettings/QuickSettingsUtil.java
@@ -44,6 +44,7 @@ import static com.android.internal.util.cm.QSConstants.TILE_SCREENTIMEOUT;
 import static com.android.internal.util.cm.QSConstants.TILE_SETTINGS;
 import static com.android.internal.util.cm.QSConstants.TILE_SLEEP;
 import static com.android.internal.util.cm.QSConstants.TILE_SYNC;
+import static com.android.internal.util.cm.QSConstants.TILE_THEMES;
 import static com.android.internal.util.cm.QSConstants.TILE_TORCH;
 import static com.android.internal.util.cm.QSConstants.TILE_USER;
 import static com.android.internal.util.cm.QSConstants.TILE_VOLUME;
@@ -177,6 +178,9 @@ public class QuickSettingsUtil {
         registerTile(new QuickSettingsUtil.TileInfo(
                 TILE_QUICKRECORD, R.string.title_tile_quick_record,
                 "com.android.systemui:drawable/ic_qs_quickrecord"));
+        registerTile(new QuickSettingsUtil.TileInfo(
+                TILE_THEMES, R.string.title_tile_themes,
+                "com.android.systemui:drawable/ic_qs_themes"));
     }
 
     private static void registerTile(QuickSettingsUtil.TileInfo info) {
