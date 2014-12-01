778af57c652453c9652ff119b645b39c819e67dc
Diogo Ferreira <defer@cyngn.com>
2014-12-01 18:17:47 +0000

    display: Make advanced settings a category
    This was a separate screen but it's too empty for that, let's make
    it a category like in CM11 for now. If it ever becomes a separate
    screen it needs its own fragment so that navigation is possible
    via actionbar.
    
    Change-Id: I4b1f3e78a013412d5e39847683ca622dc76b9fa1
    
    Conflicts:
    	res/xml/display_settings.xml

--- src/com/android/settings/DisplaySettings.java
+++ src/com/android/settings/DisplaySettings.java
@@ -50,6 +50,7 @@ import android.preference.EditTextPreference;
 import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.Preference.OnPreferenceClickListener;
+import android.preference.PreferenceCategory;
 import android.preference.PreferenceManager;
 import android.preference.PreferenceScreen;
 import android.preference.SwitchPreference;
@@ -197,7 +198,7 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
             removePreference(KEY_DISPLAY_ROTATION);
         }
 
-        PreferenceScreen advancedPrefs = (PreferenceScreen) findPreference(CATEGORY_ADVANCED);
+        PreferenceCategory advancedPrefs = (PreferenceCategory) findPreference(CATEGORY_ADVANCED);
 
         mTapToWake = (SwitchPreference) findPreference(KEY_TAP_TO_WAKE);
         if (!isTapToWakeSupported()) {
