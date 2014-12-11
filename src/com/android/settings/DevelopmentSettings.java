ec25314f9d6df5a6b94559395bda08648e0870dd
Lars Greiss <kufikugel@googlemail.com>
2014-12-11 18:20:40 +0100

    Settings: make unaccounted and over-counted battery usage configurable
    Google disabled it for release builds here:
    
    https://github.com/SlimRoms/packages_apps_Settings/commit/e6793771cedb47aed72f1c64f870b70357746938
    
    well but it is enabled for whatever other type of builds beside the official release builds. That said it should
    be a developer option so or so and not build type dependent.
    
    Whatever added an option into developer settings to control it. Default value is false.
    
    That said.....finally no complain about misc battery stat in battery settings xD
    
    Change-Id: I7549b1c796d5d9413b4af5d80a31d6efeaf0352a

--- src/com/android/settings/DevelopmentSettings.java
+++ src/com/android/settings/DevelopmentSettings.java
@@ -97,6 +97,11 @@ public class DevelopmentSettings extends SettingsPreferenceFragment
      */
     public static final String PREF_SHOW = "show";
 
+    /**
+     * Whether to show unnacounted and over-counted battery stats.  Default is false.
+     */
+    public static final String SHOW_UNAC_AND_OVERCOUNTED_STATS = "show_unac_and_overcounted_stats";
+
     private static final String ENABLE_ADB = "enable_adb";
     private static final String ADB_NOTIFY = "adb_notify";
     private static final String ADB_TCPIP = "adb_over_network";
@@ -200,6 +205,7 @@ public class DevelopmentSettings extends SettingsPreferenceFragment
     private boolean mHaveDebugSettings;
     private boolean mDontPokeProperties;
 
+    private SwitchPreference mShowUnacAndOvercounted;
     private SwitchPreference mEnableAdb;
     private SwitchPreference mAdbNotify;
     private SwitchPreference mAdbOverNetwork;
@@ -304,6 +310,8 @@ public class DevelopmentSettings extends SettingsPreferenceFragment
         }
 
         addPreferencesFromResource(R.xml.development_prefs);
+
+        mShowUnacAndOvercounted = findAndInitSwitchPref(SHOW_UNAC_AND_OVERCOUNTED_STATS);
 
         final PreferenceGroup debugDebuggingCategory = (PreferenceGroup)
                 findPreference(DEBUG_DEBUGGING_CATEGORY_KEY);
@@ -720,6 +728,7 @@ public class DevelopmentSettings extends SettingsPreferenceFragment
         updateAllOptions();
         mDontPokeProperties = false;
         pokeSystemProperties();
+        mShowUnacAndOvercounted.setChecked(false);
     }
 
     private void updateRootAccessOptions() {
