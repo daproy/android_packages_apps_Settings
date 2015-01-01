8bf522f4f556284d42d7ed90ffd7b2c7f3357da9
Janson Kang <temasek71@gmail.com>
2015-01-01 16:20:15 +0800

    Revert "[2/2] Settings: Configurable overflow menu button"
    This reverts commit 376b9d4f740ef9c785d82853394abde791e896d7.
    
    Conflicts:
    	res/values/cr_strings.xml
    	src/com/android/settings/cyanogenmod/ButtonSettings.java

--- src/com/android/settings/cyanogenmod/ButtonSettings.java
+++ src/com/android/settings/cyanogenmod/ButtonSettings.java
@@ -43,8 +43,6 @@ import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.Utils;
 
-import android.widget.Toast;
-
 import org.cyanogenmod.hardware.KeyDisabler;
 
 import com.android.internal.util.crdroid.CrUtils;
@@ -60,7 +58,6 @@ public class ButtonSettings extends SettingsPreferenceFragment implements
     private static final String KEY_ENABLE_NAVIGATION_BAR = "enable_nav_bar";
     private static final String KEY_ENABLE_HW_KEYS = "enable_hw_keys";
     private static final String KEY_NAVIGATION_BAR_LEFT = "navigation_bar_left";
-    private static final String KEYS_OVERFLOW_BUTTON = "keys_overflow_button";
     private static final String KEY_ASSIST_PRESS = "hardware_keys_assist_press";
     private static final String KEY_ASSIST_LONG_PRESS = "hardware_keys_assist_long_press";
     private static final String KEY_APP_SWITCH_PRESS = "hardware_keys_app_switch_press";
@@ -112,7 +109,6 @@ public class ButtonSettings extends SettingsPreferenceFragment implements
     private SwitchPreference mEnableNavigationBar;
     private SwitchPreference mEnableHwKeys;
     private SwitchPreference mNavigationBarLeftPref;
-    private ListPreference mOverflowButtonMode;
     private ListPreference mAssistPressAction;
     private ListPreference mAssistLongPressAction;
     private ListPreference mAppSwitchPressAction;
@@ -199,9 +195,6 @@ public class ButtonSettings extends SettingsPreferenceFragment implements
                     getPreferenceScreen().findPreference(CATEGORY_HW_KEYS);
             getPreferenceScreen().removePreference(hwKeysPref);
         }
-
-        mOverflowButtonMode = (ListPreference) findPreference(KEYS_OVERFLOW_BUTTON);
-        mOverflowButtonMode.setOnPreferenceChangeListener(this);
 
         // Navigation bar left
         mNavigationBarLeftPref = (SwitchPreference) findPreference(KEY_NAVIGATION_BAR_LEFT);
@@ -383,11 +376,6 @@ public class ButtonSettings extends SettingsPreferenceFragment implements
                 CrUtils.isNavBarDefault(getActivity()) ? 1 : 0) == 1;
         mEnableNavigationBar.setChecked(enableNavigationBar);
 
-        String overflowButtonMode = Integer.toString(Settings.System.getInt(getContentResolver(),
-                Settings.System.UI_OVERFLOW_BUTTON, 2));
-        mOverflowButtonMode.setValue(overflowButtonMode);
-        mOverflowButtonMode.setSummary(mOverflowButtonMode.getEntry());
-
         updateNavbarPreferences(enableNavigationBar);
     }
 
@@ -401,11 +389,6 @@ public class ButtonSettings extends SettingsPreferenceFragment implements
             Settings.System.putInt(getActivity().getContentResolver(),
                     Settings.System.NAVIGATION_BAR_SHOW,
                         ((Boolean) newValue) ? 1 : 0);
-            // Enable overflow button
-            Settings.System.putInt(getContentResolver(), Settings.System.UI_OVERFLOW_BUTTON, 2);
-            if (mOverflowButtonMode != null) {
-                mOverflowButtonMode.setSummary(mOverflowButtonMode.getEntries()[2]);
-            }
             return true;
         } else if (preference == mEnableHwKeys) {
             boolean hWkeysValue = (Boolean) newValue;
@@ -445,13 +428,6 @@ public class ButtonSettings extends SettingsPreferenceFragment implements
         } else if (preference == mAppSwitchLongPressAction) {
             handleActionListChange(mAppSwitchLongPressAction, newValue,
                     Settings.System.KEY_APP_SWITCH_LONG_PRESS_ACTION);
-            return true;
-        } else if (preference == mOverflowButtonMode) {
-            int val = Integer.parseInt((String) newValue);
-            int index = mOverflowButtonMode.findIndexOfValue((String) newValue);
-            Settings.System.putInt(getContentResolver(), Settings.System.UI_OVERFLOW_BUTTON, val);
-            mOverflowButtonMode.setSummary(mOverflowButtonMode.getEntries()[index]);
-            Toast.makeText(getActivity(), R.string.keys_overflow_toast, Toast.LENGTH_LONG).show();
             return true;
         }
         return false;
