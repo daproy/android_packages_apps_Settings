e5ba1cdadc633da821ea06723bb42c033d0b8f14
Janson Kang <temasek71@gmail.com>
2015-01-02 09:21:33 +0800

    Move status bar out of temasek spare parts.

--- src/com/android/settings/cyanogenmod/StatusBarSettings.java
+++ src/com/android/settings/cyanogenmod/StatusBarSettings.java
@@ -13,7 +13,7 @@
 * See the License for the specific language governing permissions and
 * limitations under the License.
 */
-package com.android.settings.temasek;
+package com.android.settings.cyanogenmod;
 
 import android.os.Bundle;
 import android.os.UserHandle;
