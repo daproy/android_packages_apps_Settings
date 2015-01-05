2d521a9003ac4a7d950432536d5ebd8458e2e394
Ricardo Cerqueira <ricardo@cyngn.com>
2015-01-05 15:03:31 +0000

    Utils: Fix isPackageInstalled
    The original implementation ignored the package state, and this is
    necessary for enable/disable toggles like the one used by Terminal.
    
    Bring the original behavior back, and make the new one optional
    
    Change-Id: I7a71f26322f4fa76dc2ae8664b3065bd6719cf11

--- src/com/android/settings/Utils.java
+++ src/com/android/settings/Utils.java
@@ -1072,11 +1072,11 @@ public final class Utils {
         return sb.toString();
     }
 
-    public static boolean isPackageInstalled(Context context, String pkg) {
+    public static boolean isPackageInstalled(Context context, String pkg, boolean ignoreState) {
         if (pkg != null) {
             try {
                 PackageInfo pi = context.getPackageManager().getPackageInfo(pkg, 0);
-                if (!pi.applicationInfo.enabled) {
+                if (!pi.applicationInfo.enabled && !ignoreState) {
                     return false;
                 }
             } catch (NameNotFoundException e) {
@@ -1085,5 +1085,9 @@ public final class Utils {
         }
 
         return true;
+    }
+
+    public static boolean isPackageInstalled(Context context, String pkg) {
+        return isPackageInstalled(context, pkg, true);
     }
 }
