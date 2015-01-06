d2b00080dff663356e7ca475387ddd472f7b86d9
Evisceration <alexander.martinz@nameless-rom.org>
2015-01-06 18:47:33 +0100

    DisplaySettings: set lcd density maximum and minimum via system properties
     * Maximum: ro.sf.lcd_density.max (default 480)
     * Minimum: ro.sf.lcd_density.min (default 280)
    
    Change-Id: I4e171cdeece2706b80b0303de8e0777946fb49c2
    Signed-off-by: Evisceration <alexander.martinz@nameless-rom.org>
    
    Conflicts:
    
    	res/values/config.xml

--- src/com/android/settings/DisplaySettings.java
+++ src/com/android/settings/DisplaySettings.java
@@ -83,6 +83,8 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
     private static final int FALLBACK_SCREEN_TIMEOUT_VALUE = 30000;
 
     private static final String PROP_DISPLAY_DENSITY = "persist.sf.lcd_density";
+    private static final String PROP_DISPLAY_DENSITY_MAX = "ro.sf.lcd_density.max";
+    private static final String PROP_DISPLAY_DENSITY_MIN = "ro.sf.lcd_density.min";
 
     private static final String KEY_SCREEN_TIMEOUT = "screen_timeout";
     private static final String KEY_FONT_SIZE = "font_size";
@@ -597,12 +599,12 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
             Settings.Secure.putInt(getContentResolver(), DOZE_ENABLED, value ? 1 : 0);
         }
         if (KEY_DISPLAY_DENSITY.equals(key)) {
-            final int max = getResources().getInteger(R.integer.display_density_max);
-            final int min = getResources().getInteger(R.integer.display_density_min);
+            final int max = SystemProperties.getInt(PROP_DISPLAY_DENSITY_MAX, 480);
+            final int min = SystemProperties.getInt(PROP_DISPLAY_DENSITY_MIN, 240);
 
             int value = SystemProperties.getInt(PROP_DISPLAY_DENSITY, 0);
             try {
-                value = Integer.parseInt((String) objValue);
+                value = Integer.parseInt(String.valueOf(objValue));
             } catch (NumberFormatException e) {
                 Log.e(TAG, "Invalid input", e);
             }
