1ed56a95b243217b92a6b94dc7e7079a00b713f0
Janson Kang <temasek71@gmail.com>
2015-01-13 17:26:03 +0800

    Revert "Settings: add back button in actionbar on Status settings"
    This reverts commit 52545af357d1fcae7c2ee6446eaeeb0deb16de3e.
    
    Duplicate. Use CM's version
    
    Conflicts:
    	src/com/android/settings/deviceinfo/Status.java

--- src/com/android/settings/deviceinfo/Status.java
+++ src/com/android/settings/deviceinfo/Status.java
@@ -17,7 +17,6 @@
 package com.android.settings.deviceinfo;
 
 import android.app.ActionBar;
-import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.content.BroadcastReceiver;
 import android.content.ClipboardManager;
@@ -293,12 +292,6 @@ public class Status extends PreferenceActivity {
         mRes = getResources();
         mUnknown = mRes.getString(R.string.device_info_default);
         mUnavailable = mRes.getString(R.string.status_unavailable);
-
-        ActionBar actionBar = getActionBar();
-        if (actionBar != null) {
-            // android.R.id.home will be triggered in onOptionsItemSelected()
-            actionBar.setDisplayHomeAsUpEnabled(true);
-        }
 
         if (UserHandle.myUserId() == UserHandle.USER_OWNER &&
                 (!isMultiSimEnabled())) {
@@ -694,24 +687,5 @@ public class Status extends PreferenceActivity {
         }
 
         return Build.SERIAL;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        final int itemId = item.getItemId();
-        switch (itemId) {
-            case android.R.id.home:
-                goUpToTopLevelSetting(this);
-                return true;
-            default:
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-    /**
-     * Finish current Activity and go up to the top level Settings.
-     */
-    private static void goUpToTopLevelSetting(Activity activity) {
-        activity.finish();
     }
 }
