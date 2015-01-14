38fe0e445c7830ab05af73b224dc0bd17771fd86
LorDClockaN <davor@losinj.com>
2015-01-14 16:38:25 -0200

    Add custom option for DPI selection
    PS2: only numbers for EditText
    
    Change-Id: I1af75f276ea4691acd06cb3ed2089d3d828e390c

--- src/com/android/settings/temasek/DisplaySettings.java
+++ src/com/android/settings/temasek/DisplaySettings.java
@@ -16,7 +16,11 @@
 package com.android.settings.temasek;
 
 import android.app.ActivityManagerNative;
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.app.IActivityManager;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.os.RemoteException;
 import android.os.ServiceManager;
@@ -24,8 +28,12 @@ import android.os.SystemProperties;
 import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.Preference.OnPreferenceChangeListener;
+import android.text.Editable;
 import android.util.DisplayMetrics;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
@@ -35,22 +43,30 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
 
     private static final String TAG = "DisplaySettings";
 
+    private static final int DIALOG_DENSITY = 101;
+
     private static final String KEY_LCD_DENSITY = "lcd_density";
 
     private ListPreference mLcdDensityPreference;
+
+    protected Context mContext;
 
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
         addPreferencesFromResource(R.xml.temasek_display_settings);
 
+        mContext = getActivity().getApplicationContext();
+        int newDensityValue;
+
         mLcdDensityPreference = (ListPreference) findPreference(KEY_LCD_DENSITY);
         int defaultDensity = DisplayMetrics.DENSITY_DEVICE;
-        String[] densityEntries = new String[8];
+        String[] densityEntries = new String[9];
         for (int idx = 0; idx < 8; ++idx) {
             int pct = (75 + idx*5);
             densityEntries[idx] = Integer.toString(defaultDensity * pct / 100);
         }
+        densityEntries[8] = getString(R.string.custom_density);
         int currentDensity = DisplayMetrics.DENSITY_CURRENT;
         mLcdDensityPreference.setEntries(densityEntries);
         mLcdDensityPreference.setEntryValues(densityEntries);
@@ -68,9 +84,14 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
     public boolean onPreferenceChange(Preference preference, Object objValue) {
         final String key = preference.getKey();
         if (KEY_LCD_DENSITY.equals(key)) {
-            int value = Integer.parseInt((String) objValue);
-            writeLcdDensityPreference(value);
-            updateLcdDensityPreferenceDescription(value);
+            String strValue = (String) objValue;
+            if (strValue.equals(getResources().getString(R.string.custom_density))) {
+                showDialog(DIALOG_DENSITY);
+            } else {
+                int value = Integer.parseInt((String) objValue);
+                writeLcdDensityPreference(value);
+                updateLcdDensityPreferenceDescription(value);
+            }
         }
         return false;
     }
@@ -104,5 +125,51 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
         catch (RemoteException e) {
             Log.e(TAG, "Failed to restart");
         }
+    }
+
+    public Dialog onCreateDialog(int dialogId) {
+        LayoutInflater factory = LayoutInflater.from(mContext);
+
+        switch (dialogId) {
+            case DIALOG_DENSITY:
+                final View textEntryView = factory.inflate(
+                        R.layout.alert_dialog_text_entry, null);
+                return new AlertDialog.Builder(getActivity())
+                        .setTitle(R.string.custom_density_dialog_title)
+                        .setMessage(getResources().getString(R.string.custom_density_dialog_summary))
+                        .setView(textEntryView)
+                        .setPositiveButton(getResources().getString(R.string.set_custom_density_set), new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int whichButton) {
+                                EditText dpi = (EditText) textEntryView.findViewById(R.id.dpi_edit);
+                                Editable text = dpi.getText();
+                                Log.i(TAG, text.toString());
+                                String editText = dpi.getText().toString();
+
+                                try {
+                                    SystemProperties.set("persist.sys.lcd_density", editText);
+                                }
+                                catch (Exception e) {
+                                    Log.w(TAG, "Unable to save LCD density");
+                                }
+                                try {
+                                    final IActivityManager am = ActivityManagerNative.asInterface(ServiceManager.checkService("activity"));
+                                    if (am != null) {
+                                        am.restart();
+                                    }
+                                }
+                                catch (RemoteException e) {
+                                    Log.e(TAG, "Failed to restart");
+                                }
+                            }
+
+                        })
+                        .setNegativeButton(getResources().getString(R.string.cancel), new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int whichButton) {
+
+                                dialog.dismiss();
+                            }
+                        }).create();
+        }
+        return null;
     }
 }
