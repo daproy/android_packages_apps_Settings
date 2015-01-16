eb63e64bf123dbde17a826129997bce9bcab1f51
Janson Kang <temasek71@gmail.com>
2015-01-16 10:10:25 +0800

    Fix trigger SystemUI restart to "Hide Dialer from lockscreen"

--- src/com/android/settings/temasek/LockMisc.java
+++ src/com/android/settings/temasek/LockMisc.java
@@ -112,7 +112,8 @@ public class LockMisc extends SettingsPreferenceFragment
             boolean value = (Boolean) objValue;
             Settings.System.putIntForUser(getActivity().getContentResolver(),
                     Settings.System.DIALER_WIDGET_HIDE, value ? 1 : 0, UserHandle.USER_CURRENT);
-            Helpers.restartSystemUI();
+            Helpers.restartSystem();
+            return true;
         }
         return false;
     }
