9042c5eca99cda121159d2727ce58c86d885a075
Janson Kang <temasek71@gmail.com>
2015-02-13 16:10:51 +0800

    Navbar Dimen: Clean up code after ScreenType FWB commit

--- src/com/android/settings/temasek/NavBarDimensions.java
+++ src/com/android/settings/temasek/NavBarDimensions.java
@@ -35,7 +35,7 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 
-import com.android.internal.util.crdroid.DeviceUtils;
+import com.android.internal.util.cm.ScreenType;
 
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.R;
@@ -71,7 +71,7 @@ public class NavBarDimensions extends SettingsPreferenceFragment implements
 
         mNavigationBarWidth =
             (ListPreference) findPreference(PREF_NAVIGATION_BAR_WIDTH);
-        if (!DeviceUtils.isPhone(getActivity())) {
+        if (!ScreenType.isPhone(getActivity())) {
             prefSet.removePreference(mNavigationBarWidth);
             mNavigationBarWidth = null;
         } else {
